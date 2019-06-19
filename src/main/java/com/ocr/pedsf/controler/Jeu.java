2ee825f58b7030b3f47ee4256aa70c5efc81468f
PEDSF1968 <pe.dsf@free.fr>
2019-06-19 17:44:16 +0200

    Package controler - class Jeu implémentation de la méthode duel

--- src/main/java/com/ocr/pedsf/controler/Jeu.java
+++ src/main/java/com/ocr/pedsf/controler/Jeu.java
@@ -14,6 +14,9 @@ public class Jeu {
      demarrage();
    }
 
+   /**
+    * demarrage : méthode de lancement de l'accueil pour choisir le mode de jeu ou la taille du code
+    */
    protected void demarrage(){
       int mode = affichage.choixDuMode();
 
@@ -38,23 +41,27 @@ public class Jeu {
    }
 
 
+   /**
+    * challenger : méthode de gestion du mode challenger où l'utilisateur doit trouver le code de l'ordinateur
+    */
    private void challenger(){
       System.out.println("\nMASTERMIND : Mode challenger");
       boolean trouve = false;
       int nbCoup = 0;
       String proposition = "";
-      NombreSecret ns = new NombreSecret(mp.getLongueur());
+      // initialisation du nombre de l'ordinateur
+      NombreSecret nso = new NombreSecret(mp.getLongueur());
 
       if(mp.isModeDeveloppeur())
-         System.out.println("(Combinaison secrète : " + ns.getNombre()+")");
+         System.out.println("(Combinaison secrète : " + nso.getNombre()+")");
 
       do {
          proposition = affichage.demandeProposition(mp.getLongueur());
          nbCoup++;
 
          try {
-            System.out.println(ns.test(proposition));
-            if(proposition.equals(ns.getNombre()))
+            System.out.println(nso.test(proposition));
+            if(proposition.equals(nso.getNombre()))
                trouve = true;
 
          } catch (TailleDifferenteException e) {
@@ -65,20 +72,22 @@ public class Jeu {
       if (nbCoup<mp.getNbEssai()) {
          affichage.gagne(nbCoup);
       } else {
-         affichage.perdu(ns.getNombre());
+         affichage.perdu(nso.getNombre());
       }
 
       demarrage();
    }
 
+   /**
+    * defenseur : méthode de gestion du mode defenseur où l'utilisateur rentre un code et l'ordinateur doit trouver la solution
+    */
    private void defenseur(){
+      System.out.println("\nMASTERMIND : Mode defenseur");
       boolean trouve = false;
       int nbCoup = 0;
       String reponse = "";
       // saisie du code de départ par l'utilisateur
       NombreSecret nsu = new NombreSecret(affichage.demandeProposition(mp.getLongueur()));
-
-      System.out.println("\nMASTERMIND : Mode defenseur");
 
       IA ia = new IA(mp.getLongueur());
 
@@ -105,8 +114,57 @@ public class Jeu {
       demarrage();
    }
 
+   /**
+    * duel : méthode de gestion du mode duel où l'utilisateur et l'ordinateur doivent chacun trouver le code de l'autre
+    */
    private void duel(){
       System.out.println("\nMASTERMIND : Mode duel");
+      boolean trouve = false;
+      int nbCoup = 0;
+      String reponse = "";
+      String proposition = "";
+      // initialisation du nombre de l'ordinateur
+      NombreSecret nso = new NombreSecret(mp.getLongueur());
+
+      if(mp.isModeDeveloppeur())
+         System.out.println("(Combinaison secrète : " + nso.getNombre()+")");
+
+      // saisie du code de départ par l'utilisateur
+      NombreSecret nsu = new NombreSecret(affichage.demandeProposition(mp.getLongueur()));
+
+      IA ia = new IA(mp.getLongueur());
+
+      do {
+         // l'utilisateur demande à l'ordinateur
+         System.out.println("C'est à votre tour de trouver le code");
+         proposition = affichage.demandeProposition(mp.getLongueur());
+         try {
+            System.out.println(nso.test(proposition));
+         } catch (TailleDifferenteException e) {
+            e.printStackTrace();
+         }
+
+         if(proposition.equals(nso.getNombre())) {
+            trouve = true;
+            affichage.gagne(nbCoup);
+         } else {
+            // si la réponse n'est pas trouvée c'est au tour de l'ordinateur
+            System.out.println("C'est à l'ordinateur de trouver le code");
+           if (!reponse.equals(""))
+               ia.proposition(reponse);
+
+            System.out.println("Vous : " + nsu.getNombre());
+            System.out.println("IA : " + ia.getNombreSecret());
+            reponse = affichage.demandeReponse(mp.getLongueur());
+            nbCoup++;
+
+            if(nsu.getNombre()==ia.getNombreSecret()) {
+               trouve = true;
+               affichage.perdu(nsu.getNombre());
+            }
+         }
+      } while (!trouve);
+
       demarrage();
    }
 }
